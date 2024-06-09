3c1b85b45ebb6738c42859b56209c0027767ba60
Gabor Szabo <gabor@szabgab.com>
2024-06-09 11:27:29 +0300

    prepare site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::create_dir_all;
+
 fn main() {
-    println!("Hello, world!");
+    create_dir_all("_site").unwrap();
+    let html = r#"
+        <h1>Hello World</h1>
+    "#;
+    std::fs::write("_site/index.html", html).unwrap();
+
 }
