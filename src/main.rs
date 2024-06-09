8ce998278a9a6553efa6c3ccc38db15788b4402c
Gabor Szabo <gabor@szabgab.com>
2024-06-09 17:30:09 +0300

    markdown

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::fs::create_dir_all("_site").unwrap();
-    let html = r#"
-        <h1>Hello from Rust</h1>
-    "#;
-    std::fs::write("_site/index.html", html)?;
-    Ok(())
-}
-
 // fn main() -> Result<(), Box<dyn std::error::Error>> {
 //     std::fs::create_dir_all("_site").unwrap();
-
-//     let markdown = std::fs::read_to_string("content.md")?;
-//     match markdown2html(&markdown) {
-//         Ok(html) => std::fs::write("_site/index.html", html)?,
-//         Err(err) => return Err(Box::<dyn std::error::Error>::from(format!("{err}"))),
-//     }
-
+//     let html = r#"
+//         <h1>Hello from Rust</h1>
+//     "#;
+//     std::fs::write("_site/index.html", html)?;
 //     Ok(())
 // }
 
-// fn markdown2html(content: &str) -> Result<String, markdown::message::Message> {
-//     let html = markdown::to_html_with_options(
-//         content,
-//         &markdown::Options {
-//             compile: markdown::CompileOptions {
-//                 allow_dangerous_html: true,
-//                 ..markdown::CompileOptions::default()
-//             },
-//             ..markdown::Options::gfm()
-//         },
-//     )?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::fs::create_dir_all("_site").unwrap();
 
-//     Ok(html)
-// }
+    let markdown = std::fs::read_to_string("README.md")?;
+    match markdown2html(&markdown) {
+        Ok(html) => std::fs::write("_site/index.html", html)?,
+        Err(err) => return Err(Box::<dyn std::error::Error>::from(format!("{err}"))),
+    }
+
+    Ok(())
+}
+
+fn markdown2html(content: &str) -> Result<String, markdown::message::Message> {
+    let html = markdown::to_html_with_options(
+        content,
+        &markdown::Options {
+            compile: markdown::CompileOptions {
+                allow_dangerous_html: true,
+                ..markdown::CompileOptions::default()
+            },
+            ..markdown::Options::gfm()
+        },
+    )?;
+
+    Ok(html)
+}
